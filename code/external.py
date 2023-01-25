2e7306ed0c09727cef51dbe69878d89069acb14c
Thomas Feutren <116446502+tfeutren@users.noreply.github.com>
2023-01-25 09:21:17 +0100

    Update external coeff

--- code/external.py
+++ code/external.py
@@ -8,5 +8,10 @@ from math import exp
 # ----------------------------
 # Define External Class :
 class External:
+    '''
+    Génère un coefficient €[0,1] par un modèle exponnentiel ( il y a plus de chance d'obtenir un nombre proche de 0 que de 1
+    '''
     def __init__(self):
-        self.externalCoef = exp(-random.choice([àk for k in range(10)]))
+        self.externalCoef = exp(-random.choice([k for k in range(15)]))
+
+# ----------------------------

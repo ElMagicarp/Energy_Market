99f106b810d7a4fb6f392a2ccf9e80953194fc3a
Thomas Feutren <116446502+tfeutren@users.noreply.github.com>
2023-01-25 11:52:09 +0100

    Update external.py
    implement of pipe protocol

--- code/external.py
+++ code/external.py
@@ -3,43 +3,41 @@
 # --------------------------- #
 
 import random
-
 import os
 import signal
-import time
-from multiprocessing import Process
+from multiprocessing import Process,Pipe
 
 # ----------------------------
-# Define External Class :
+# Define External Class
 class External:
     '''
-    Génère le coefficient qui représente les facteurs extérieurs, 
+    # Coef Externe
+    Permet de savoir si un évènement extérieur s'est produit :
+    - Evènements possibles : Guerre / Petrole
+    - run : modifie la valeur d'un évènement de manière aléatoire
     '''
 
     def __init__(self):
-        self.listCoef = {"warEvent" : 0,
-                         "petrolCrisisEvent" :0}
-    
-    def run(self):
-        x = random.random()
-        if self.listCoef["warEvent"] == 0:
-            if x < 0.05 :
-                self.listCoef["warEvent"] =1
-        else:
-            if x < 0.35:
-                self.listCoef["petrolCrisisEvent"] =0
-        if self.listCoef["petrolCrisisEvent"] == 0:
-            if x < 0.05 :
-                self.listCoef["petrolCrisisEvent"] =1
-        else:
-            if x < 0.35:
-                self.listCoef["petrolCrisisEvent"] =0
-        
+        self.listCoef = {"warEvent" : 0, "petrolCrisisEvent" :0}
 
-# ----------------------------
-# Multiprocessing
+    def run(self, childPipe):
 
-def child_external():
-    os.kill(os.getpid(), signal.SIGUSR1)
-    while True:
-        monCoefExternal = External.run()
+        "Vérification changement War"
+        xWar = random.random()
+        if xWar< 0.05 :
+            if self.listCoef["warEvent"]==0:
+                self.listCoef["warEvent"]=1
+            else:
+                self.listCoef["warEvent"]=0
+            os.kill(os.getppid(), signal.SIGUSR1)
+            childPipe.send(self.listCoef["warEvent"])
+
+        "Vérification changement Petrol"
+        xPetrol = random.random()
+        if xPetrol < 0.10 :
+            if self.listCoef["petrolCrisisEvent"]==0:
+                self.listCoef["petrolCrisisEvent"]=1
+            else:
+                self.listCoef["petrolCrisisEvent"]=0
+            os.kill(os.getppid(), signal.SIGUSR2)
+            childPipe.send(self.listCoef["petrolCrisisEvent"])

f4d535252cc9aad825018e42226d793421ac0da6
amFarwati <am.farwati@gmail.com>
2023-01-25 10:45:44 +0100

    external+formule market
    fait:
    -maj external
    -calcul prix
    
    à faire:
    -maj weather
    -config socket homes
    -maj en fct homes

--- code/market.py
+++ code/market.py
@@ -1,30 +1,34 @@
 import sys
-import multiprocessing as Process
+import os
+from multiprocessing import Process, Pipe
+import signal
 import threading
 import sysv_ipc
 import socket
+import external
+import weather
 
-class internalFactors():
+class InternalFactor():
 
     def __init__(self, type ,weight, value = 0):
-
         self.type = type
         self.weight = weight
         self.value = value
 
-class externalFactors():
+
+class ExternalFactor():
 
     def __init__(self, weight, value):
-
         self.weight = weight
         self.value = value
 
 
+class Market():
 
-class market():
-
-    def __init__( self, currentEnergyPrice = 1, longtermeAttenuation = 1 , amoutEnergyBought = 0, amoutEnergySold = 0, internalFactors =[], externalFactors =[], ):
-
+    def __init__(self, currentEnergyPrice = 1, longtermeAttenuation = 1 , amoutEnergyBought = 0, amoutEnergySold = 0, internalFactors =[], externalFactors =[]):
+        signal.signal(signal.SIGUSR1, handler)
+        parentConn,childConn = Pipe()
+        self.connPipe = [parentConn,childConn]
         self.currentEnergyPrice = currentEnergyPrice
         self.longtermeAttenuation = longtermeAttenuation
         self.amoutEnergyBought = amoutEnergyBought
@@ -32,16 +36,49 @@ class market():
         self.internalFactors = internalFactors
         self.externalFactors = externalFactors
 
-    def computeContribution(self, factor):
-        
-        result=0
-        for event in factor:
-            result+=event.weight*event.value
-        return result
-
     def computeCurrentEnergyPrice(self):
+        newPrice = self.longtermeAttenuation*self.currentEnergyPrice + computeContribution(self.internalFactors) + computeContribution(self.externalFactors)
+        self.currentEnergyPrice = newPrice
 
-        newPrice = self.longtermeAttenuation*self.currentEnergyPrice + computeContribute(self.internalFactors) + computeContribute(self.externalFactors)
+
+if __name__ == '__main__':
+
+    tempFactor = InternalFactor("weather", 0.001, None)
+    warEvent = ExternalFactor(0.2, 0)
+    petrolCrisisEvent = ExternalFactor(0.05, 0)
+
+    energyMarket = Market(currentEnergyPrice=0.145, longtermeAttenuation= 0.99, internalFactors=[tempFactor], externalFactors=[warEvent,petrolCrisisEvent])
+    externalGenerator = external(os.getpid(),(energyMarket.connPipe)[1])
+    weatherGenerator = weather()
+
+    externalProcess = Process(target= externalGenerator.run, args=())
+    weatherProcess = Process(target= weatherGenerator.run, args=())
+
+    while True:
+        externalProcess.start()
+        weatherProcess.start()
+        externalProcess.join()
+        weatherProcess.join()
+
+        energyMarket.computeCurrentEnergyPrice()
+
+
+
+
+
+def handler (self,sig,frame):
+        if sig == signal.SIGUSR1:
+            self.externalFactors[0].value = self.connPipe[0].recv()
+        if sig == signal.SIGUSR2:
+            self.externalFactors[1].value = self.connPipe[0].recv()
+
+    
+def computeContribution(factor):
+    
+    result=0
+    for event in factor:
+        result+=event.weight*event.value
+    return result
 
     
 

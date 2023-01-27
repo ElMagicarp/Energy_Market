5f9e2d8ebb55dc62704c11b67e531e2fb15c4304
Thomas Feutren <116446502+tfeutren@users.noreply.github.com>
2023-01-27 12:02:22 +0100

    V2 de genHome.py
    ajout de fonctionalité dans le fichier genHome
    il faut reprendre la fonction à la fonction run

--- code/genHome.py
+++ code/genHome.py
@@ -6,8 +6,7 @@
 
 import socket
 import random
-from market import weatherFactor
-
+from .market import Market, weatherFactor, NOMBRE_MAISON
 
 # Définition de la classe "Maison"
 
@@ -19,19 +18,42 @@ class Maison:
     ## Méthodes
     - ajoutEnergie : ajoute de l'énergie à la maison (None)
     - utiliseEnergie : utilise de l'energie (None)
-    - envoieEnergie : envoie de l'énergie au marché (None)
+    - besoinEnergie : Retourne le besoin d'énergie journalier de la maison en fonction de la température et du nombre de personne
+    - demandePrix : Demande le prix du kwh jounalier
     '''
-    def __init__(self, quantiteEnergie):
+    def __init__(self, quantiteEnergie, haveSolarPanel, haveWindTurbine):
         self.quantiteEnergie = quantiteEnergie
+        self.haveSolarPanel = haveSolarPanel
+        self.haveWindTurbine = haveWindTurbine
         self.nombrePersonnes =  random.choice([1,2,2,3,3,3,4,4,4,5,5,6])
+        self.listeVoisins = listeMaison(NOMBRE_MAISON)
+        self.listBesoinJour = [( -4/30 * weatherFactor[0] + 7 ) * self.nombrePersonnes]
     
     def ajoutEnergie(self, energie):
         self.quantitieEnregie += energie
     
     def utiliseEnergie(self, energie):
         self.quantiteEnergie -= energie * self.nombrePersonnes
+        
+    def besionEnergie(self):
+        besoin = ( -4/30 * weatherFactor[0] + 7 ) * self.nombrePersonnes
+        self.listBesoinJour.append(besoin)
+        return besoin
     
-    def envoieEnergie(self):
+    def demandePrix(self):
+        return Market.currentEnergyPrice
+
+    def productionEnregie(self):
+        energie = 0
+        if self.haveSolarPanel == True:
+            energie += weatherFactor[2] * 2
+        if self.haveWindTurbine == True:
+            energie += weatherFactor[1] * 2/10
+        return energie
+        
+    
+    
+    def faireUneSocket(self):
         "Créer une socket pour se connecter au serveur"
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
             "Se connecter au serveur"
@@ -47,3 +69,10 @@ class Maison:
 
 
 def listeMaison(nombreMaison):
+    '''génère la liste des maisons'''
+    return [Maison(0,random.choice([True,False]),random.choice([True,False])) for i in range(nombreMaison)]
+
+
+def run(maison : Maison()):
+    '''Fonction Principal pour faire les opérations sur une maison'''
+    pass

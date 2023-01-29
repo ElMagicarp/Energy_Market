6de3c2d02ff54d970cb779c19814dd7919ff61ac
Thomas Feutren <116446502+tfeutren@users.noreply.github.com>
2023-01-29 23:37:52 +0100

    Update of my genHome.py
    Clean code and add the run methode about my class

--- code/genHome.py
+++ code/genHome.py
@@ -8,6 +8,7 @@ import socket
 import random
 from .market import Market, weatherFactor, NOMBRE_MAISON
 
+#-----------------------------------------------
 # Définition de la classe "Maison"
 
 class Maison:
@@ -15,11 +16,17 @@ class Maison:
     classe représentant une maison
     ## Attribut
     - quantiteEnergie : quantité d'énergie de la maison (None)
+    - haveSolarPanel : retourne Vrai si la maison possède des panneaux solaires (Bool)
+    - haveWindTurbine : retounre Faux si la maison possède une éoliène (Bool)
+    - nombrePersonnes : Nombre de personne dans une maison (int)
+    - listeVoisins : Liste des maisons dans notre système (list)
+    _ listBesoinListe : liste qui énumère pour chaque jour la consommation énergétique nécessaire (float)
     ## Méthodes
-    - ajoutEnergie : ajoute de l'énergie à la maison (None)
-    - utiliseEnergie : utilise de l'energie (None)
-    - besoinEnergie : Retourne le besoin d'énergie journalier de la maison en fonction de la température et du nombre de personne
-    - demandePrix : Demande le prix du kwh jounalier
+    - besoinEnergie : Retourne le besoin d'énergie journalier de la maison en fonction de la température et du nombre de personne (float)
+    - demandePrix : Demande le prix du kwh jounalier (float)
+    - productionEnergie : Retourne la quantité d'énergie quotidienne fournie par la maison et l'ajoute à quantiteEnergie (float)
+    - faireUneSocket : process de socket entre genHome et market
+    - run : actions quotidiennes d'une maison
     '''
     def __init__(self, quantiteEnergie, haveSolarPanel, haveWindTurbine):
         self.quantiteEnergie = quantiteEnergie
@@ -28,12 +35,6 @@ class Maison:
         self.nombrePersonnes =  random.choice([1,2,2,3,3,3,4,4,4,5,5,6])
         self.listeVoisins = listeMaison(NOMBRE_MAISON)
         self.listBesoinJour = [( -4/30 * weatherFactor[0] + 7 ) * self.nombrePersonnes]
-    
-    def ajoutEnergie(self, energie):
-        self.quantitieEnregie += energie
-    
-    def utiliseEnergie(self, energie):
-        self.quantiteEnergie -= energie * self.nombrePersonnes
         
     def besionEnergie(self):
         besoin = ( -4/30 * weatherFactor[0] + 7 ) * self.nombrePersonnes
@@ -49,6 +50,7 @@ class Maison:
             energie += weatherFactor[2] * 2
         if self.haveWindTurbine == True:
             energie += weatherFactor[1] * 2/10
+        self.quantiteEnergie += energie
         return energie
         
     
@@ -65,14 +67,34 @@ class Maison:
             print("echo> ", data.decode())
             "Fermer la connexion"
             client_socket.close()
+            
+            
+    def run(self):
+    # TODO : ajouter un système de payement ?
+        '''Fonction Principal pour faire les opérations sur une maison'''
+        self.quantiteEnergie -=  self.besionEnergie()
+        if self.quantiteEnergie < 0 :
+            "Vérifie si les voisins on des surplus d'énergie"
+            for voisin in self.listeVoisins:
+                if voisin.quantiteEnergie > 0:
+                    if self.quantiteEnergie + voisin.quantiteEnergie >0:
+                        voisin.quantiteEnergie -= self.quantiteEnergie
+                        self.quantiteEnergie = 0
+                        break
+                    else:
+                        self.quantiteEnergie += voisin.quantiteEnergie
+                        voisin.quantiteEnergie = 0
+            "Si les voisins n'ont pas assez d'energie, la maison achète de l'énerge au Market"
+            if self.quantiteEnergie < 0:
+                # TODO : achter au market l'énregie manquante quotidienne  
+                self.quantiteEnergie = 0 
+            
+                
+                    
 
-
+#-----------------------------------------------
+# Création de la liste des maisons de notre système
 
 def listeMaison(nombreMaison):
     '''génère la liste des maisons'''
     return [Maison(0,random.choice([True,False]),random.choice([True,False])) for i in range(nombreMaison)]
-
-
-def run(maison : Maison()):
-    '''Fonction Principal pour faire les opérations sur une maison'''
-    pass

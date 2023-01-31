09ac0663f70deecbc5469582cf43d64e5dbc67ac
Thomas Feutren <116446502+tfeutren@users.noreply.github.com>
2023-01-31 18:05:20 +0100

    Update stats.py

--- code/stats.py
+++ code/stats.py
@@ -17,6 +17,7 @@ from rich import box
 import matplotlib.pyplot as plt
 from math import *
 import random
+import csv
 
 # -------------------------------------
 # Affichage des Statistiques Weather
@@ -31,6 +32,13 @@ X = [k for k in range(365)]
 Ytemp = [temp(t) for t in X]
 Yens = [ens(t) for t in X]
 Yvent = [vent(t) for t in X]
+Yprice = [] 
+with open('price.csv', 'r') as f:
+    reader = csv.reader(f)
+    header = next(reader)
+    for row in reader:
+        Yprice.append(float(row[0]))
+Xprice = [k for k in range(len(Yprice))]
 
 # Affichages des graphiques
 plt.plot(X, Ytemp)
@@ -42,24 +50,27 @@ plt.plot(X, Ytemp,color='red')
 plt.xlabel('Jour')
 plt.ylabel("°C")
 plt.title("Température en fonction du jour de l'année (en °C)")
-plt.legend()
 
 plt.subplot(222)
 plt.plot(X, Yens,color='green')
 plt.xlabel('Jour')
 plt.ylabel('Taux')
 plt.title("Taux d'ensoleillement en fonction du jour de l'année")
-plt.legend()
 
 plt.subplot(223)
 plt.plot(X, Yvent,color='darkblue')
 plt.xlabel('Jour')
 plt.ylabel('indice')
 plt.title("Indice de vent en fonction du jour de l'année")
-plt.legend()
+
+plt.subplot(224)
+plt.plot(Xprice, Yprice,color='purple')
+plt.xlabel('Jour')
+plt.ylabel('€/kWh')
+plt.title("Prix de l'énegrie en fonction du jour de l'année")
 
 plt.tight_layout()
-# plt.show()
+plt.show()
 
 # -------------------------------------
 # Utilisation d'une interface terminal avec "rich"
@@ -71,18 +82,22 @@ tableMaison = Table(title="[red]Les Maisons")
 
 "Création des colonnes"
 tableMaison.add_column("ID", justify="center", style="cyan")
-tableMaison.add_column("Cout Energie", justify="center", style="magenta")
-tableMaison.add_column("Quantité Energie", justify="center", style="#45dc81")
 tableMaison.add_column("Panneaux Solaires", justify="center", style="red")
 tableMaison.add_column("Eolienne", justify="center", style="#0b2b8a")
 tableMaison.add_column("Pikachu", justify="center", style="yellow")
 tableMaison.add_column("Nombre Personnes", justify="center", style="#eeeeee")
 
+"Lire le fichier csv"
+M = [] # Liste des données data.csv
+with open('data.csv', 'r') as f:
+    reader = csv.reader(f)
+    header = next(reader)
+    for row in reader:
+        M.append(row)
 
 "Création des lignes"
-
-tableMaison.add_row("1", "2", "3", "4", "5", "6", "7")
-
+for maison in M:
+    tableMaison.add_row(maison[0], maison[1], maison[2], maison[3], maison[4])
 
 "Affichage du tableau"
 console = Console()

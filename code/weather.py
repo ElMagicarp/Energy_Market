9b69b5f1b95d3777a911ae62ad25782103d4759f
amFarwati <am.farwati@gmail.com>
2023-02-01 16:35:32 +0100

    mise en forme v.1

--- code/weather.py
+++ code/weather.py
@@ -7,22 +7,22 @@ import random
 import threading
 from math import *
 
-# -------------------------------------------
+# ----------------------------------------------------------------------------------------------
 # NOTES
 "On suppose à t=0 on est le 1er janvier et que à t=365 on est le 31 décembre"
 "Certaine données sont imspirées des valeurs métérologiques de la ville de LYON :"
 "https://fr.wikipedia.org/wiki/Lyon"
 
-# -------------------------------------------
-# CLASSES METEO
+# ----------------------------------------------------------------------------------------------
+#_CLASSE_METEO_---------------------------------------------------------------------------------
 class Weather():
     '''
     ## Arguments:
     - t : jour de l'année (int)
     ## Méthodes: 
-    - dataJour(self, list, sem) : met à jour les paramétres de meteo (None)
+    - dataJour(self, sharedMemory, sem) : met à jour les paramétres de meteo (None)
     - jourAnnee(self) : renvoie le jour de l'année (list)
-    - tempJour(self,list,waitData) : calcule la température du jour (None)
+    - tempJour(self,sharedMemory,waitData) : calcule la température du jour (None)
     - ensJour() : calcule l'ensoleillement du jour (None)
     - ventJour() : calcule le vent du jour (None)
     - afficheStatJour() : affiche les statistiques du jour (str)
@@ -31,20 +31,18 @@ class Weather():
     def __init__(self,t):
         self.t = t
     
-    #mise à jour paramétres de meteo 
-    def dataJour(self, list, sem):
-
-        #print("Starting process dataJour\n")
+    #_mise_à_jour_paramètres_de_météo_--------------------------------------------------------- 
+    def dataJour(self, sharedMemory, sem):
         
-        self.t=list[3]
+        self.t=sharedMemory[3]
 
         waitTemp = Semaphore(0)
         waitWind = Semaphore(0)
         waitSunBeam = Semaphore(0)
 
-        temperature = threading.Thread(target=self.tempJour, args=(list,waitTemp,))
-        windSpeed = threading.Thread(target=self.ventJour, args=(list,waitWind,))
-        sunbeam = threading.Thread(target=self.ensJour, args=(list,waitSunBeam,))
+        temperature = threading.Thread(target=self.tempJour, args=(sharedMemory,waitTemp,))
+        windSpeed = threading.Thread(target=self.ventJour, args=(sharedMemory,waitWind,))
+        sunbeam = threading.Thread(target=self.ensJour, args=(sharedMemory,waitSunBeam,))
 
         temperature.start()
         windSpeed.start()
@@ -61,7 +59,7 @@ class Weather():
 
         print("Ending process dataJour\n")
 
-    # Définition du jour de l'année en fonction de t
+    #_définition_du_jour_de_l'année_en_fonction_de_t_--------------------------------------------
     def jourAnnee(self):
         mois = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"]
         nbJoursMois = [31,28,31,30,31,30,31,31,30,31,30,31]
@@ -72,35 +70,35 @@ class Weather():
             else:
                 return (jour, mois[k])
 
-    # Définition de la température quotidienne
+    #_définition_de_la_température_quotidienne_---------------------------------------------------
     "Température en °C"
-    def tempJour(self,list,waitData):
+    def tempJour(self,sharedMemory,waitData):
         #print("Starting thread:", threading.current_thread().name)
         coefSaison = -sin(2*pi*self.t/365-250)*15 + 14.5 
         bruit = random.random()*5*random.randint(-1,1)
-        list[0]= coefSaison + bruit
+        sharedMemory[0]= coefSaison + bruit
         waitData.release()
         #print("Ending thread:", threading.current_thread().name)
 
     # Définition de l'ensoleillement moyen en 24h
     "Taux d'ensoleillement entre 0 et 1"
-    def ensJour(self,list,waitData):
+    def ensJour(self,sharedMemory,waitData):
         #print("Starting thread:", threading.current_thread().name)
         (heuresEnsAnnee,heureAnnee) = (2001.9, 8760)
         fmoy = heuresEnsAnnee/heureAnnee
         coefsaison = fmoy - 0.1*sin(2*pi*self.t/365-250)
         bruit = random.random()*0.075*random.randint(-1,1)
-        list[2] = coefsaison + bruit
+        sharedMemory[2] = coefsaison + bruit
         waitData.release()
         #print("Ending thread:", threading.current_thread().name)
     
     # Définition du vent moyen en 24h
     "Indice entre 0 et 10"
-    def ventJour(self,list,waitData):
+    def ventJour(self,sharedMemory,waitData):
         #print("Starting thread:", threading.current_thread().name)
         coefSaison = 5 + 2*sin(2*pi*self.t/365-250)
         bruit = random.random()*3*random.randint(-1,1)
-        list[1]=  coefSaison + bruit
+        sharedMemory[1]=  coefSaison + bruit
         waitData.release()
         #print("Ending thread:", threading.current_thread().name)
     
